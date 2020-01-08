00d92dda996c371371bc47f4cb8b2e1c80ebac8f
liuxiang <350646436@qq.com>
2020-01-08 16:39:16 +0800

    f

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,3 @@
 import http from '@shared/http'
-import storage from '@shared/storage'
-
-http.domain = SERVER_HOST
-http.errorHandler = function(code, message) {
-  console.log(message)
-}
-
-storage.prefix = `sz-${ENV}`
 
 export const login = params => http.post('/api/user/login', params)
