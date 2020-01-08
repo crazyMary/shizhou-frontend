1d8e10f8f9a9f94107a8075b965b04de8efb582c
liuxiang <350646436@qq.com>
2020-01-08 16:44:04 +0800

    f

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,11 +2,14 @@ import '@shared/global.scss'
 import './index.scss'
 import '@shared/init'
 import storage from '@shared/storage'
+import { useEffect } from 'react'
 
-API.login({ username: 'liuxiang', password: '123456' }).then(res => {
-  storage.setItem('token', res.token)
-})
 function App() {
+  useEffect(() => {
+    API.login({ username: 'liuxiang', password: '123456' }).then(res => {
+      storage.setItem('token', res.token)
+    })
+  }, [])
   return (
     <div id="LoginPage" className="wrapper">
       {ENV}
